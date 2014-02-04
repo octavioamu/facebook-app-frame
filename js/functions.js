e367352f62ab9d4297a9d3378c55750e259bd129
Octavio Amuchastegui <octavioamuchastegui@gmail.com>
2014-02-04 14:29:11 -0200

    Update visual button search

--- js/functions.js
+++ js/functions.js
@@ -1,12 +1,16 @@
     function inputKeyUp(e) {
       function urlSet(urlSrc) {
         var iframeElement = document.getElementById('iframeid');
-        iframeElement.src = "http://" + urlSrc;
+        iframeElement.src = urlSrc;
 
       }
       e.which = e.which || e.keyCode;
       if(e.which == 13) {
+        if (document.getElementById('urlInput').value) {
           var urlInput =  document.getElementById('urlInput');
           urlSet(document.getElementById("urlInput").value);
+        } else {
+          alert('Need to write an url');
+        }
       }
     }
